304163eb870467c2cbdc3ce3f85ae9202fedc5ac
Ningensei848 <k.kubokawa@klis.tsukuba.ac.jp>
2022-01-15 20:18:26 +0900

    Add: sitemap.xml

---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://ningensei848.github.io',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+    outDir: './out'
+}
