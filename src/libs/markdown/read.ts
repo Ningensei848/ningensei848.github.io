04fa0e685b5304232b5f40cf4355d3b5fc40a21d
Ningensei848 <k.kubokawa@klis.tsukuba.ac.jp>
2022-01-16 19:07:58 +0900

    Update: approach to get commit logs

--- src/libs/markdown/read.ts
+++ src/libs/markdown/read.ts
@@ -88,10 +88,10 @@ export const isSameFilepaths = (elderArray: string[], newerArray: string[]) => {
 const JSON_FILEPATH_LIST = `${PROJECT_ROOT}/.next/commitLog.json`
 const promisedExec = promisify(exec)
 const gitLog = (slug: string, ext: string) => {
-  const filepath = [MARKDOWN_DIR, `${slug}.${ext.replaceAll(/\./g, '')}`].join(sep)
+  const filepath = [MARKDOWN_DIR, `${slug}.${ext.replace(/^\./, '')}`].join(sep)
   // Note: GitHub Actions の `actions/checkout@v2` は fetch.depth = 1 である
   // 方針：最新の履歴だけでなく，リモートリポジトリ内の過去 10 件の履歴も得る
-  return `git log --remotes=origin -n 10 --pretty=oneline --pretty=format:"[%H] %ad (%ar) || %s" ${filepath}`
+  return `git log --author Ningensei848 --remotes=origin --branches=main -n 10 --follow --pretty=format:"[%H] %ad (%ar) || %s" -- ${filepath}`
 }
 
 /* blogs/ 以下を再帰的に走査し，md および mdx のファイルパス一覧を得る
